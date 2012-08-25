84fb89604882978195ed5b0780971983923c1078
Nirut Khemasakchai <rutcreate@gmail.com>
2012-08-25 19:42:45 +0700

    - add debt adapter - change main activity to load data from new adapter - add textview amount to list_item layout

--- src/com/example/debtmanager/MainActivity.java
+++ src/com/example/debtmanager/MainActivity.java
@@ -4,18 +4,16 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.example.debtmanager.widget.DebtAdapter;
 
 public class MainActivity extends Activity {
 
 	private ListView mListView;
-	private ArrayAdapter<String> mAdapter;
-	private Cursor mCursor;
-	private ArrayList<String> mData;
+	private DebtAdapter mAdapter;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -23,22 +21,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		this.mListView = (ListView) this.findViewById(R.id.list_view);
-
-		DatabaseHelper dbHelper = new DatabaseHelper(this);
-		SQLiteDatabase db = dbHelper.getReadableDatabase();
-		this.mCursor = db.rawQuery(
-				"SELECT name, amount FROM data ORDER BY _id DESC", null);
-
-		this.mData = new ArrayList<String>();
-		this.mCursor.moveToFirst();
-		do {
-			this.mData.add(this.mCursor.getString(0) + " "
-					+ this.mCursor.getString(1));
-		} while (this.mCursor.moveToNext());
-
-		this.mAdapter = new ArrayAdapter<String>(this, R.layout.list_item,
-				R.id.title, this.mData);
-
+		this.mAdapter = new DebtAdapter(this);
 		this.mListView.setAdapter(this.mAdapter);
 	}
 
---
+++ src/com/example/debtmanager/widget/DebtAdapter.java
@@ -0,0 +1,88 @@
+package com.example.debtmanager.widget;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.debtmanager.DatabaseHelper;
+import com.example.debtmanager.model.Debt;
+import com.example.debtmanager.R;
+
+public class DebtAdapter extends BaseAdapter {
+
+	private Context mContext;
+	private List<Debt> mDataSource;
+
+	public DebtAdapter(Context pContext) {
+		super();
+		this.mContext = pContext;
+
+		DatabaseHelper dbHelper = new DatabaseHelper(this.mContext);
+		SQLiteDatabase db = dbHelper.getReadableDatabase();
+		Cursor cursor = db.rawQuery(
+				"SELECT _id, name, amount FROM data ORDER BY _id DESC", null);
+		this.mDataSource = new ArrayList<Debt>();
+		while (cursor.moveToNext()) {
+			final int id = cursor.getInt(0);
+			final String name = cursor.getString(1);
+			final Double amount = cursor.getDouble(2);
+			Debt debt = new Debt(id, name, amount);
+			this.mDataSource.add(debt);
+		}
+	}
+
+	@Override
+	public int getCount() {
+		return this.mDataSource.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return this.mDataSource.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		Debt debt = (Debt) this.mDataSource.get(position);
+		return debt.getId();
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder viewHolder = null;
+		if (convertView == null) {
+			viewHolder = new ViewHolder();
+			convertView = LayoutInflater.from(this.mContext).inflate(
+					R.layout.list_item, parent, false);
+
+			viewHolder.name = (TextView) convertView.findViewById(R.id.title);
+			viewHolder.amount = (TextView) convertView
+					.findViewById(R.id.amount);
+
+			convertView.setTag(viewHolder);
+		} else {
+			viewHolder = (ViewHolder) convertView.getTag();
+		}
+		
+		Debt debt = (Debt) this.getItem(position);
+		
+		viewHolder.name.setText(debt.getName());
+		viewHolder.amount.setText(debt.getAmount().toString());
+
+		return convertView;
+	}
+
+	private class ViewHolder {
+		TextView name;
+		TextView amount;
+	}
+
+}
