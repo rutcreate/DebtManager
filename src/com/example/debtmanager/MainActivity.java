9ec8552284944da97b4270757242f19ed8d30e23
Nirut Khemasakchai <rutcreate@gmail.com>
2012-08-25 19:53:22 +0700

    handle add menu item

--- src/com/example/debtmanager/MainActivity.java
+++ src/com/example/debtmanager/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.debtmanager;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.debtmanager.widget.DebtAdapter;
 
@@ -14,7 +13,7 @@ public class MainActivity extends Activity {
 
 	private ListView mListView;
 	private DebtAdapter mAdapter;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,5 +28,16 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_add:
+			Toast.makeText(this, "You clicked at \"Add\" menu item",
+					Toast.LENGTH_LONG).show();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
